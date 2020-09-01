b4cbdd39340676ad5a83da38f8899447ce3b74cc
Rodrigo 'Pinguim' de Oliveira <casa.rodrigo@gmail.com>
2020-08-31 21:39:55 -0300

    Ajuste listen

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ app.use(routes);
 
 
 //app.listen(process.env.PORT || 3333, () => {
-    app.listen(3333, () => {    
+    app.listen(process.env.PORT || 3333, () => {    
     console.log('🚀 Server ControlPay online!');
   });
