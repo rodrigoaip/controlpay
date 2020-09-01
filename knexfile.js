f61452060fafbab5500cfd6af6218cd1cb678e94
Rodrigo 'Pinguim' de Oliveira <casa.rodrigo@gmail.com>
2020-09-01 09:16:39 -0300

    Ajuste Conexao Producao

--- knexfile.js
+++ knexfile.js
@@ -37,13 +37,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      host: process.env.PG_HOST || 'ec2-3-213-102-175.compute-1.amazonaws.com',
-      port: process.env.PG_PORT ||  '5432',
-      database:process.env.PG_DB ||'d6hn2sdg2ee0r0',
-      user: process.env.PG_USER || 'kwbvejtdouxquu',
-      password:  process.env.PG_PASSWORD || '9a920c8cfec3e27f5f21481fbec96591e0bc63f7400e34910d96b31623fce139'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
