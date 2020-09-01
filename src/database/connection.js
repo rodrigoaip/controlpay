5ebebef724ef7f7ee6e0e413a1ddeeedcdb9d711
Rodrigo 'Pinguim' de Oliveira <casa.rodrigo@gmail.com>
2020-09-01 09:38:04 -0300

    Ajuste Conexao Producao

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,9 @@
+const environment = process.env.NODE_ENV || 'development';
 const knex = require('knex');
-const configuration = require('../../knexfile');
+const configuration = require('../../knexfile')[environment];
 
-const connection = knex(configuration.development);
+//const connection = knex(configuration.development);
+const connection = knex(configuration);
 
 module.exports = connection;
+
